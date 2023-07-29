a586f8f0dcbb011393a33ad4b43bf5ec51cf12a1
Rwear <20172333127@m.scnu.edu.cn>
2023-07-28 23:38:43 -0400

    Initial commit

---
+++ src/main/java/com/yupi/project/service/InterfaceInfoService.java
@@ -0,0 +1,13 @@
+package com.yupi.project.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.yupi.project.model.entity.InterfaceInfo;
+
+/**
+* @author 27143
+* @description 针对表【interface_info(接口信息)】的数据库操作Service
+* @createDate 2023-07-28 18:43:31
+*/
+public interface InterfaceInfoService extends IService<InterfaceInfo> {
+
+}
